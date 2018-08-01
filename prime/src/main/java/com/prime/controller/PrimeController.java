697209d4dc9888b810082b6bff5c8e2c450aaa3a
Suiyc <sui0619@163.com>
2018-08-01 22:25:16 +0800

    prime

--- prime/src/main/java/com/prime/controller/PrimeController.java
+++ prime/src/main/java/com/prime/controller/PrimeController.java
@@ -1,9 +1,11 @@
 package com.prime.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeController {
+    @RequestMapping("/prime")
     public String prime(){
         return "这是一个SpringBoot项目的完美开端！";
     }
