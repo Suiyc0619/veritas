8d1b517b2ffb15879fc652a582602cb430a29a02
sui <sui0619@163.com>
2018-08-02 14:06:51 +0800

    覆盖

--- prime/src/main/java/com/prime/controller/PrimeController.java
+++ prime/src/main/java/com/prime/controller/PrimeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PrimeController {
     @RequestMapping("/prime")
     public String prime(){
-        return "这是一个SpringBoot项目的完美开端！";
+        return "这是一个SpringBoot项目的完美开端！Git测试。";
     }
 }
