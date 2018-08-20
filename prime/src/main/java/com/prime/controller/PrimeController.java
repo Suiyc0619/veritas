113321d0bf2077b64048d5cdd27ac4f570b7c049
sui <sui0619@163.com>
2018-08-20 10:29:46 +0800

    覆盖

--- prime/src/main/java/com/prime/controller/PrimeController.java
+++ prime/src/main/java/com/prime/controller/PrimeController.java
@@ -1,5 +1,7 @@
 package com.prime.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.prime.model.Person;
 import com.prime.model.PersonCon;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PrimeController {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     Person person;
     @Autowired
     PersonCon personCon;
 
     @RequestMapping("/prime")
-    public Person prime(){
+    public String prime() {
+        logger.trace("通配符测试...");
         System.out.println(personCon.person());
-        return person;
+//        return person;
+        return "这是一个SpringBoot项目的完美开端！Git测试。增量包测试！";
+
     }
+
 }
--- prime/src/main/java/com/prime/model/Person.java
+++ prime/src/main/java/com/prime/model/Person.java
@@ -2,11 +2,6 @@ package com.prime.model;
 
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.stereotype.Component;
 
 
 @ConfigurationProperties(prefix = "person")
