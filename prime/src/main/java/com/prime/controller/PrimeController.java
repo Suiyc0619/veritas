d550ed12f0bce5b32606bc2e6ee50420faecdacf
sui <sui0619@163.com>
2018-08-22 17:58:09 +0800

    覆盖

--- prime/src/main/java/com/prime/controller/PrimeController.java
+++ prime/src/main/java/com/prime/controller/PrimeController.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import com.prime.model.Person;
 import com.prime.model.PersonCon;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 @RestController
 public class PrimeController {
@@ -19,8 +21,10 @@ public class PrimeController {
     PersonCon personCon;
 
     @RequestMapping("/prime")
-    public String prime() {
-        logger.trace("通配符测试...");
+    public String prime(HttpServletRequest request) {
+        logger.trace("通配符测试...a");
+        request.getParameter("");
+        Locale locale = new Locale("","");
         System.out.println(personCon.person());
 //        return person;
         return "这是一个SpringBoot项目的完美开端！Git测试。增量包测试！";
